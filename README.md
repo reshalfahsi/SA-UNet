b79cdde3bb0484fe413e631e2d88f17f1ec79dae
reshalfahsi <resha.alfahsi@gmail.com>
2022-12-28 21:01:33 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# SA-UNet: Spatial Attention U-Net for Retinal Vessel Segmentation
+
+This is an unofficial PyTorch implementation of [SA-UNet](https://github.com/clguo/SA-UNet).
+
+
+## Requirements
+
+```
+pip install -r requirements.txt
+```
+
+
+## Model
+
+
+<div align="center">
+
+<img src="https://raw.githubusercontent.com/clguo/SA-UNet/master/SA-UNet.png" width="800">
+
+</div>
+
+Diagram of SA-UNet architecture.
---
+++ saunet.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision.ops import drop_block2d
+
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat(
+            (torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1
+        )
+
+
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = nn.Sequential(
+            nn.Conv2d(2, 1, kernel_size, padding=(kernel_size - 1) // 2),
+            nn.BatchNorm2d(1),
+        )
+
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = torch.sigmoid(x_out)  # broadcasting
+        return x * scale
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None, attention=False):
+        super().__init__()
+
+        if not mid_channels:
+            mid_channels = out_channels
+
+        self.conv1 = nn.Conv2d(
+            in_channels, mid_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bnrelu1 = nn.Sequential(
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+        )
+
+        self.attention = SpatialGate() if attention else nn.Identity()
+
+        self.conv2 = nn.Conv2d(
+            mid_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bnrelu2 = nn.Sequential(
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = drop_block2d(x, 0.18, 7, inplace=True, training=self.training)
+        x = self.bnrelu1(x)
+        x = self.attention(x)
+        x = self.conv2(x)
+        x = drop_block2d(x, 0.18, 7, inplace=True, training=self.training)
+        x = self.bnrelu2(x)
+        return x
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels, attention=False):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels, attention=attention),
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(
+            in_channels, in_channels // 2, kernel_size=2, stride=2
+        )
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class SAUNet(nn.Module):
+    def __init__(self):
+        super(SAUNet, self).__init__()
+
+        self.inc = DoubleConv(3, 16)
+
+        self.down1 = Down(16, 32)
+        self.down2 = Down(32, 64)
+        self.down3 = Down(64, 128, attention=True)
+
+        self.up1 = Up(128, 64)
+        self.up2 = Up(64, 32)
+        self.up3 = Up(32, 16)
+
+        self.outc = OutConv(16, 1)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+
+        logits = self.outc(x)
+        return logits
+
+
+if __name__ == "__main__":
+    from ptflops import get_model_complexity_info
+
+    model = SAUNet()
+
+    macs, params = get_model_complexity_info(
+        model, (3, 224, 224), as_strings=True, print_per_layer_stat=True, verbose=True
+    )
+    print("{:<30}  {:<8}".format("Computational complexity: ", macs))
+    print("{:<30}  {:<8}".format("Number of parameters: ", params))
